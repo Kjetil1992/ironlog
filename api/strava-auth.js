99f92f64d8495d876c1be6966b40f4bfd9aa0c5a
Kjetil1992 <kjetilhauan@outlook.com>
2026-05-26 22:20:18 +0200

    Change Strava scope to activity:read

--- api/strava-auth.js
+++ api/strava-auth.js
@@ -6,7 +6,7 @@ module.exports = function handler(req, res) {
   if (!clientId) return res.status(500).json({ error: 'STRAVA_CLIENT_ID not configured' })
 
   const redirectUri = 'https://ironlog-phi-liard.vercel.app/api/strava-callback'
-  const url = `https://www.strava.com/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=activity:read_all&state=${user_id}`
+  const url = `https://www.strava.com/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=activity:read&state=${user_id}`
 
   res.redirect(url)
 }
